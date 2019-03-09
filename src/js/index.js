47f8aef774c7528792fc20ee001c8a4b9508ce97
JupiterandSaturn <986774896@qq.com>
2019-03-09 22:52:35 +0800

    小图轮播实现

--- src/js/index.js
+++ src/js/index.js
@@ -1,134 +1,121 @@
-$(function(){
-	
-	
-	$bussinessCard = $('#bussiniss-Card');
-	$bussinessCard.click(function(){
-		//这里还没添加跳转页面
-		location.href = "";
-	})
-	
-	$select5lux = $('.select-5lux');
-	$list5lux = $('.list-5lux');
-	$list5luxa = $('.list-5lux dd a');
-	$.each($list5luxa,function(){
-		$(this).mouseenter(function(){
-		$(this).css('color','red');
-		})
-	})
-	$.each($list5luxa,function(){
-		$(this).mouseleave(function(){
-		$(this).css('color','#635f60');
-		})
-	})
-	
-	$select5lux.mouseenter(function(){
-		$list5lux.css('display','block');
-	})
-	
-	$list5lux.mouseleave(function(){
-		$list5lux.css('display','none');
-	})
-	
-	//头部移入事件，手机下载
-	$phoneDownload = $('.phone-download');
-	$phoneErweima = $('.phone-erweima');
-	$phoneDownload.mouseenter(function(){
-		$phoneErweima.css('display','block');
-	})
-		$phoneDownload.mouseleave(function(){
-		$phoneErweima.css('display','none');
-	})
-	$phoneErweima.mouseenter(function(){
-		$phoneErweima.css('display','block');
-	})
-	$phoneErweima.mouseleave(function(){
-		$phoneErweima.css('display','none');
-	})
-	
-	//搜索框：jsonP获取
-	//请求 URL: https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=&cb=
-	$search = $('#search');
-	$searchvalue = $('.search-value');
-	
-	$search.keyup(function(){
-		$text =  $search.val();
-		$.getJSON('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd='+$text+'&cb=?',function(data){
-			$arr = data.s;
-			console.log($arr);
-			$searchvalue.html('');
-			$.each($arr,function(index,value){
-				
-				$li = $('<li></li>').html($arr[index]).css('background','white');
-				$searchvalue.append($li);
-				
-			})
-		})
-		
-	})
-	$searchvalue.click(function(){
-		$searchvalue.css('display','none');
-	})
-	
-//	banner轮播图
+$(function() {
+
+	//	banner轮播图
 	$imageBanner = $('#imageBanner .image-list li');
+	$borderindex = $('#imageBanner .border-index li');
 	$btnLeft = $('#btnLeft');
 	$btnRight = $('#btnRight');
-	var index = 5;
+	let index = 5;
 	var timer = null;
-	
-	$btnLeft.mouseenter(function(){
+
+	$btnLeft.mouseenter(function() {
 		clearInterval(timer);
 	})
-	
-	$btnRight.mouseenter(function(){
+
+	$btnRight.mouseenter(function() {
 		clearInterval(timer);
 	})
-	$btnLeft.mouseleave(function(){
+	$btnLeft.mouseleave(function() {
 		autoPlay();
 	})
-	$btnRight.mouseleave(function(){
+	$btnRight.mouseleave(function() {
 		autoPlay();
 	})
-	
-	
-	$btnLeft.click(function(){
-		index --;
-		if(index === -1){
+
+	$btnLeft.click(function() {
+		index--;
+		if(index === -1) {
 			index = $imageBanner.size() - 1;
 		}
 		slide();
 	})
-	
-	$btnRight.click(function(){
-		index ++;
-		if(index === $imageBanner.size()){
+
+	$btnRight.click(function() {
+		index++;
+		if(index === $imageBanner.size()) {
 			index = 0;
 		}
 		slide();
 	})
-	
+
 	slide();
 	autoPlay();
-	function slide(){
-		
+
+	function slide() {
+
 		//$imageBanner.eq(index).fadeIn(1500).siblings().fadeOut(1500);
-		$imageBanner.eq(index).animate({left:'-100%'},1000);
-		if(index == 0){
-			$imageBanner.css('left','0');
-		}else {
-			$imageBanner.eq(0).css('left','0');
-			
+		$imageBanner.eq(index).animate({
+			left: '-100%'
+		}, 1000);
+		if(index == 0) {
+			$imageBanner.css('left', '0');
+			$borderindex.css({background:'white',border:'1px solid white'});
+		} else {
+			$imageBanner.eq(0).css('left', '0');
+			$borderindex.eq(0).css({background:'white',border:'1px solid white'});
 		}
-		
+
+		$borderindex.eq(index).css({
+			'background': 'red',
+			'border': '1px solid red'
+		});
+
 	}
-	function autoPlay(){
-		timer = setInterval(function(){
+
+	function autoPlay() {
+		timer = setInterval(function() {
 			index--;
-			if(index === -1){
+			if(index === -1) {
 				index = $imageBanner.size() - 1;
 			}
 			slide();
-		},3000)
+		}, 3000)
 	}
+	///-----------------------------------------------------///
+	$blandpic = $('#flagShipBrand .bland-pic li span');
+	$.each($blandpic, function() {
+		$(this).mouseenter(function() {
+			$(this).animate({
+				top: '180px'
+			}, 500);
+
+		})
+		$(this).mouseleave(function() {
+			$(this).animate({
+				top: '260px'
+			}, 500)
+		})
+	});
+
+	//小图轮播
+	$SmallBtnLeft = $('#flagShipBrand .hot-store .right');
+	$SmallBtnRight = $('#flagShipBrand .hot-store .left');
+	$smallpic = $('#flagShipBrand .bland-list');
+	$SmallBtnLeft.click(function(){
+		$smallpic.animate({
+			left: '-1203px'
+			
+		}, 1000);
+		$SmallBtnLeft.click(function() {
+		$smallpic.animate({
+			left: '0'
+			
+		}, 1000);
+		
+	})
+	})
+	
+	$SmallBtnRight.click(function() {
+		$smallpic.animate({
+			left: '-1203px'
+		}, 1000);
+		$SmallBtnRight.click(function() {
+		$smallpic.animate({
+			left: '-2406px'
+			
+		}, 1000);
+		
+	})
+	})
 	
 })
---
+++ src/js/public.js
@@ -0,0 +1,96 @@
+$(function(){
+$bussinessCard = $('#bussiniss-Card');
+	$bussinessCard.click(function(){
+		//这里还没添加跳转页面
+		location.href = "";
+	})
+	
+	$select5lux = $('.select-5lux');
+	$list5lux = $('.list-5lux');
+	$list5luxa = $('.list-5lux dd a');
+	$.each($list5luxa,function(){
+		$(this).mouseenter(function(){
+		$(this).css('color','red');
+		})
+	})
+	$.each($list5luxa,function(){
+		$(this).mouseleave(function(){
+		$(this).css('color','#635f60');
+		})
+	})
+	
+	$select5lux.mouseenter(function(){
+		$list5lux.css('display','block');
+	})
+	
+	$list5lux.mouseleave(function(){
+		$list5lux.css('display','none');
+	})
+	
+	//头部移入事件，手机下载
+	$phoneDownload = $('.phone-download');
+	$phoneErweima = $('.phone-erweima');
+	$phoneDownload.mouseenter(function(){
+		$phoneErweima.css('display','block');
+	})
+		$phoneDownload.mouseleave(function(){
+		$phoneErweima.css('display','none');
+	})
+	$phoneErweima.mouseenter(function(){
+		$phoneErweima.css('display','block');
+	})
+	$phoneErweima.mouseleave(function(){
+		$phoneErweima.css('display','none');
+	})
+	
+	//搜索框：jsonP获取
+	//请求 URL: https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=&cb=
+	$search = $('#search');
+	$searchvalue = $('.search-value');
+	
+	$search.keyup(function(){
+		$text =  $search.val();
+		$.getJSON('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd='+$text+'&cb=?',function(data){
+			$arr = data.s;
+			console.log($arr);
+			$searchvalue.html('');
+			$.each($arr,function(index,value){
+				
+				$li = $('<li></li>').html($arr[index]).css('background','white');
+				$searchvalue.append($li);
+				
+			})
+		})
+		
+	})
+	$searchvalue.click(function(){
+		$searchvalue.css('display','none');
+	})
+	
+
+	
+	
+	//nav导航栏的下拉菜单
+	$selectList = $('#selectList');
+	$firstList = $('#selectList .first-list');
+	$navList = $('#productNavigation .produce-nav .nav-list');
+	$selecttwo = $('#selectTwo');
+	$bannerMark = $('.banner-Mark');
+	$navList.mouseenter(function(){
+		$bannerMark.css('opacity','0.3');
+		$selectList.css('display','block');
+		$firstList.mouseenter(function(){
+			$selecttwo.css('display','block');
+		})
+		
+		$selectList.mouseleave(function(){
+			$selecttwo.mouseleave(function(){
+				$selectList.css('display','none');
+				$selecttwo.css('display','none');
+				$bannerMark.css('opacity','0');
+			})
+		})
+	})
+	
+	
+})
